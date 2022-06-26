0d79200e7a8a2e703f0e49654de20fa2ca455b04
koki <kkttm530@gmail.com>
2022-06-26 17:30:08 +0900

    language switch func add

---
+++ backends/textData.js
@@ -0,0 +1,25 @@
+
+/**
+ * DRFで作成したAPIへPOSTを実行
+ * @param postText monaco editor内のテキストデータ
+ * @param id UUID
+ * @param lang monaco editorで指定している言語
+ * @returns 実行結果
+ */
+export async function post(postText, id, lang) {
+    const url = process.env.NEXT_PUBLIC_BASE_API_URL + "/data_manage/";
+
+    const data = { text: postText, id: id, lang: lang };
+
+    const res = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+
+    // TODO: error処理を
+    return true
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,34 @@
 import Editor from "@monaco-editor/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import { v4 } from "uuid";
 
+import style from "../styles/Index.module.css";
+import { post as dataPost } from "../backends/textData";
+
 export default function MonacoEditor({}) {
   const editorRef = useRef(null);
-  /**monaco editorの初期化*/
+  const monacoRef = useRef(null);
+  const [generateLink, setGenerateLink] = useState("");
+  const [editorLang, setEditorLang] =useState("Plain Text")
+  /**対応言語 */
+  const options = ["Plain Text", "javascript", "python", "css"];
+
+  /**monaco editorの初期化　editor, monacoインスタンスはonMount時に引数として*/
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
+    monacoRef.current = monaco;
   }
 
-  // function showValue() {
-  //   alert(editorRef.current.getValue());
-  //   console.log(editorRef.current.getValue())
-  // }
+  /**monaco editorの言語を変更する */
+  function updateEditorLang(lang) {
+    monacoRef.current.editor.setModelLanguage(
+      editorRef.current.getModel(),
+      lang
+    );
+    // 更新
+    setEditorLang(lang)
+  }
 
   /**monaco editorに記述されているテキストを取得 */
   function getValue() {
@@ -23,51 +38,65 @@ export default function MonacoEditor({}) {
 
   /**ユーザー用にURLを生成 */
   function generatePageUrl(id) {
-    const url =
-      process.env.NEXT_PUBLIC_BASE_URL + "/display/" + id + "/";
+    const url = process.env.NEXT_PUBLIC_BASE_URL + "/display/" + id + "/";
+    setGenerateLink(url);
     return url;
   }
 
-  /**記述したテキストをPOSTする */
-  async function postData() {
-    const url = process.env.NEXT_PUBLIC_BASE_API_URL + "/data_manage/";
+  /**クリック時に全選択の状態にする */
+  function selectLink() {
+    document.getElementById("select-link").select();
+  }
 
-    console.log(data);
+  /**入力データを保存。表示用のリンク取得 */
+  function post() {
     // generate uuid
     const id = v4();
-    console.log(id);
 
-    const data = { text: getValue(), id: id };
-    const body = JSON.stringify(data);
-    console.log("body", body);
+    // get value
+    const postText = getValue();
 
-    const res = await fetch(url, {
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    // if post success
-    const link = generatePageUrl(id);
-    alert(link);
-
-    console.log("res", res);
-
-    const json = await res.json();
-    return json;
+    /**記述したテキストをPOSTする */
+    const res = dataPost(postText, id, editorLang);
+    if (res) {
+      generatePageUrl(id);
+    }
   }
 
   return (
     <div>
-      <button onClick={getValue}>get value</button>
-      <button onClick={postData}>generate link</button>
+      <div className={`${style.text_title}`}>NoPaste Like App</div>
+
+      <button onClick={post}>共有用のリンクを取得</button>
+      <select
+        onChange={(e) => updateEditorLang(options[e.target.selectedIndex])}
+        defaultValue={options[0]}
+      >
+        {options.map((option, id) => (
+          <option key={id} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      <input
+        defaultValue={generateLink}
+        onClick={selectLink}
+        id="select-link"
+      ></input>
       <Editor
         height="90vh"
-        defaultLanguage="javascript"
-        defaultValue="// some comment"
+        width="100vw"
+        defaultLanguage=""
+        defaultValue=""
+        options={{
+          minimap: { enabled: false },
+          overviewRulerLanes: 0,
+          scrollbar: {
+            vertical: "hidden",
+            horizontal: "hidden",
+            handleMouseWheel: false,
+          },
+        }}
         onMount={handleEditorDidMount}
       />
     </div>
